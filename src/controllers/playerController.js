b6f60e95034afe710a395fa1cc8070ca28930895
Rodrigo Kunzler Weber <rodrigo.weber@appmax.com.br>
2025-08-23 20:28:33 -0300

    feat: Add controller Player

---
+++ src/controllers/playerController.js
@@ -0,0 +1,31 @@
+import {
+  createPlayer,
+  getPlayerById,
+  listPlayers,
+} from "../models/playerModel";
+
+export const create = (req, res) => {
+  const { fullName, age, hight, weight, position, team } = req.body;
+
+  if (!fullName) {
+    return res.status(400).json({ error: "All required fields" });
+  }
+
+  const player = createPlayer({ fullName, age, hight, weight, position, team });
+  res.status(201).json(player);
+};
+
+export const getById = (req, res) => {
+  const { id } = req.params;
+  const player = getPlayerById(id);
+
+  if (!player) {
+    return res.status(404).json({ error: "Player is not found" });
+  }
+
+  res.json(player);
+};
+
+export const list = (req, res) => {
+  res.json(listPlayers);
+};
